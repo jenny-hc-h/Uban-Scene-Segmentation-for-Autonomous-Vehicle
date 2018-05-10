705fa9652b317ddebd8cb8c1944ec7892cedbba5
Jenny Huang <34726738+jenny30825@users.noreply.github.com>
2018-05-09 23:28:30 -0700

    load new dataset

--- sceneSeg.py
+++ sceneSeg.py
@@ -3,43 +3,52 @@ import numpy as np
 import glob 
 import os
 import tqdm
+import matplotlib.pyplot as plt
+import scipy.io as spio
+import scipy.misc as spmi
 
-CLASSES = [33, 34, 35, 36, 38, 39, 40] #car, motorcycle, bicycle, pedestrian, truck, bus, tricycle
-N_EXAMPLE = 10
-N_TRAINING_DATA = 8
+LEARNING_RATE = 0.01
+BATCH_SIZE = 10
+MAX_STEP = 100
+N_EXAMPLE = 1 # maximum 3475
+N_TRAINING_DATA = 1
+IMAGE_SCALE = 0.25
 
 
 def load_dataset(dataset_path,N_examples,N_traingdata):
+    """
+    Cityscapes Dataset : https://www.cityscapes-dataset.com/
+    """
     labelset = []
     imageset = []
-    im_fpath = glob.glob(dataset_path+"/train_color/*.jpg")
+    im_fpath = glob.glob(dataset_path+"/leftImg8bit/*.png")
     for n in tqdm.tqdm(range(0,N_examples)):
-        # load labels --- NxWxH
-        lb_fn = im_fpath[n].split('/')
-        lb_fn = os.path.splitext(lb_fn[-1])[0] + '_instanceIds.png'
-        print lb_fn
-        label = np.array(Image.open(dataset_path+"/train_label/"+lb_fn))
-        print np.unique(label)
-        class_numbers = np.unique(label/1000)
-        class_based = np.zeros(label.shape)
-        for i in class_numbers:
-            if i in CLASSES:
-                loc = (label/1000) == i
-                class_based[loc] = (CLASSES.index(i) + 1)
-        labelset.append(class_based)
-        print np.unique(class_based) 
-        # load images --- NxWxHx3
-        image = np.array(Image.open(im_fpath[n]))
-        imageset.append(image)
+        # load labels --- N x H x W x C(19)
+        lb_fn = os.path.splitext(im_fpath[n].split('/')[-1])[0][0:-12] + '_gtFine_color.mat'
+        labelset.append(np.array(spio.loadmat(dataset_path+"/gtFine/"+lb_fn)['label']))
+        # load images --- N x H x HW x 3
+        image = Image.open(im_fpath[n])
+        imageset.append(np.array(spmi.imresize(image, (int(image.size[1]*IMAGE_SCALE),int(image.size[0]*IMAGE_SCALE),3), interp='bilinear')))
+
+    """................................test label.................................
+    fig, ax = plt.subplots(1, 1)
+    plt.axis('off')
+    im =  np.array(spmi.imresize(image, (int(image.size[1]*IMAGE_SCALE),int(image.size[0]*IMAGE_SCALE),3), interp='bilinear'))
+    ax.imshow(im)
+    label = np.array(spio.loadmat(dataset_path+"/gtFine/"+lb_fn)['label'])
+    ax.imshow(label[:,:,13], alpha=0.3)
+    plt.show()
+    ................................test label................................."""
 
     # Split the dataset into training and testing sets
-    train_data = np.array(imageset[0:N_traingdata]) # NxWxHx3
+    train_data = np.array(imageset[0:N_traingdata]) 
     test_data = np.array(imageset[N_traingdata:N_examples])
-    train_labels = np.array(labelset[0:N_traingdata]) # NxWxH
+    train_labels = np.array(labelset[0:N_traingdata])
     test_labels = np.array(labelset[N_traingdata:N_examples])
+    print train_data.shape, train_labels.shape
     return {"train_data":train_data, "test_data": test_data, "train_labels": train_labels, "test_labels": test_labels}
     
-
-load_dataset("/Users/JennyH/Desktop/Jenny/UCSD/HW/MachineLearning/Dataset", N_EXAMPLE, N_TRAINING_DATA)
+dataset_path = "/Users/JennyH/Desktop/Jenny/UCSD/HW/MachineLearning/CityscapesDataset"
+load_dataset(dataset_path, N_EXAMPLE, N_TRAINING_DATA)
 
 
